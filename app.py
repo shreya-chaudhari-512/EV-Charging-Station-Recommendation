bd44f21fd1612da67f2e83001665c5caffb86047
Shreya Chaudhari <143866457+shreya-chaudhari-512@users.noreply.github.com>
2025-04-27 19:41:31 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import pydeck as pdk
-import joblib
-import matplotlib.pyplot as plt
-import seaborn as sns
+st.write('hello')
 
 
 
