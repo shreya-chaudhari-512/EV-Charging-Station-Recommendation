dcc523ffdeb76eb51e4d1e368f0c30d23b9a540c
Shreya Chaudhari <143866457+shreya-chaudhari-512@users.noreply.github.com>
2025-04-27 19:40:03 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
